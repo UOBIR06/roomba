ef8bbc381d4d31bdd5f24b896eb4e02122defc07
MLuleci <mert@luleci.net>
2021-12-10 17:42:53 +0000

    Fixed idle time calculation

--- scripts/coverage.py
+++ scripts/coverage.py
@@ -3,29 +3,34 @@
 import os
 import rospy
 import datetime
-from geometry_msgs.msg import Twist
-from nav_msgs.msg import OccupancyGrid
+from geometry_msgs.msg import Pose, Twist
+from nav_msgs.msg import OccupancyGrid, Odometry
 from roomba import util
 
 
 class Coverage(object):
     def __init__(self):
         self.map_sub = rospy.Subscriber('/map', OccupancyGrid, self.map_callback)
-        self.vel_sub = rospy.Subscriber('/cmd_vel', Twist, self.vel_callback)
+        self.vel_sub = rospy.Subscriber('/base_pose_ground_truth', Odometry, self.odom_callback)
+        self.start_time = rospy.get_time()
         self.map_data = []
         self.idle_time = 0
         self.was_idle = True
         self.last_idle = rospy.get_time()
+        self.last_pose = Pose()
 
-    def vel_callback(self, msg: Twist):
-        angular = any([msg.angular.x, msg.angular.y, msg.angular.z])
-        linear = any([msg.linear.x, msg.linear.y, msg.linear.z])
-        active = angular or linear
+    def odom_callback(self, msg: Odometry):
+        pose = msg.pose.pose
+        active = abs(self.last_pose.position.x - pose.position.x) > 0 \
+                 or abs(self.last_pose.position.y - pose.position.y) > 0 \
+                 or abs(util.getHeading(pose.orientation) - util.getHeading(self.last_pose.orientation)) > 0
 
+        self.last_pose = pose
         if active and self.was_idle:  # Started moving again
             self.was_idle = False
             self.idle_time += rospy.get_time() - self.last_idle
         elif not active and not self.was_idle:  # Stopped moving
+            self.was_idle = True
             self.last_idle = rospy.get_time()
 
     def map_callback(self, msg: OccupancyGrid):
@@ -34,7 +39,9 @@ class Coverage(object):
         self.map_data.append((time, count))
 
     def flush(self):
-        rospy.loginfo(f'Spent {self.idle_time} seconds idle')
+        total = rospy.get_time() - self.start_time
+        rospy.loginfo(f'!!! STATS !!!\n\tIdle time: {self.idle_time}\n\tTotal time: {total}')
+
         output = ''
         for t, d in self.map_data:
             output += f'{t},{d}\n'
